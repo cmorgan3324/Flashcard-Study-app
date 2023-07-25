433756497ba54fa4a63d6b6379ee6a27b18ad52c
Cory Morgan <132379707+cmorgan3324@users.noreply.github.com>
2023-07-25 18:26:03 -0400

    Clean Study.js

--- Flashcard App code/src/Home/Study.js
+++ Flashcard App code/src/Home/Study.js
@@ -3,50 +3,47 @@ import React, { useEffect, useState } from "react";
 import { Link, useParams, useHistory } from "react-router-dom";
 import { readDeck } from "../utils/api";
 import { readCard } from "../utils/api";
-//import ErrorMessage from "./ErrorMessage";
 
-export const Study = () => {
+
+function Study() {
   const initialFormState = {
     front: "",
-
     back: "",
   };
-
   const [content, setContent] = useState({ ...initialFormState });
-  const history = useHistory();
-
-  const [deck, setDeck] = useState({ cards: [] }); //initialize deck to empty deck of cards
-  const { deckId } = useParams();
+  const [deck, setDeck] = useState({ cards: [] }); // Initialize deck to empty deck of cards
   const [card, setCard] = useState({ ...initialFormState });
   const [cardIndex, setCardIndex] = useState(0);
   const [isFront, setIsFront] = useState([]);
+  
+  const history = useHistory();
+  const { deckId } = useParams();
 
+  // Load deck
   useEffect(() => {
     async function getDeck() {
       const deck = await readDeck(deckId);
       setDeck(deck);
       setCard(deck.cards[0]);
-      //console.log(deck);
     }
-
     getDeck();
   }, [deckId]);
 
+  // Flip button handler
   const handleFlip = async (id) => {
     setIsFront((currentValue) => !currentValue);
     return;
   };
 
-  //need to increment the card index
-
+  //Next button handler; need to increment card index
   const handleNext = async (id) => {
     setCardIndex((currentValue) => currentValue + 1);
     setIsFront(true);
     setCard(deck.cards[cardIndex + 1]);
-
+    
     if (cardIndex === deck.cards.length - 1) {
       const result = window.confirm("Restart cards?");
-
+      
       if (result) {
         setCardIndex(0);
         setIsFront(true);
@@ -59,13 +56,11 @@ export const Study = () => {
     return;
   };
 
+  // Add card button handler
   const handleAddCards = async (id) => {
     return history.push(`/decks/${deck.id}/cards/new`);
   };
 
-  // if (!deck) {
-  //   return <ErrorMessage />;
-  // } else
   if (deck.cards.length > 2) {
     return (
       <div>
@@ -82,7 +77,6 @@ export const Study = () => {
             </li>
           </ol>
         </nav>
-
         <div className="border p-4 h-100 d-flex flex-column">
           <div>
             <h6>
@@ -91,14 +85,11 @@ export const Study = () => {
             </h6>
             <h3 className="display-6 mb-6">Study: {deck.name}</h3>
           </div>
-
           <p>{isFront ? card.front : card.back}</p>
-
           <div className="d-grid gap-6 d-md-block">
             <button className="btn btn-secondary" onClick={() => handleFlip()}>
               Flip
             </button>
-
             {!isFront && (
               <button className="btn btn-primary" onClick={() => handleNext()}>
                 Next
